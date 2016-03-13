3616b038c5c18767a139b14a1fe21d4fa9badc2b
Peter Strömberg <pez@pezius.com>
2016-03-13 10:18:35 +0100

    Use multimethods for dispatching the routes

--- src/routing_example/core.cljs
+++ src/routing_example/core.cljs
@@ -3,71 +3,87 @@
             [reagent.session :as session]
 
             [bidi.bidi :as bidi]
-            ;;[schema.core :as s] ;For when redfining routes get tricky
+            ;;[schema.core :as s] ;For when defining routes get tricky
             ;;[bidi.schema]
 
             [accountant.core :as accountant]))
 
+(enable-console-print!)
+
 (def app-routes
   ["/"
    [["" :index]
-    ["section-a/"
+    ["section-a"
      [["" :section-a]
-      [["item/" :item-id] :section-a-item]]]
-    ["section-b/" :section-b]
+      [["/item-" :item-id] :a-item]]]
+    ["section-b" :section-b]
+    ["missing-route" :missing-route]
     [true :four-o-four]]])
 
 ;;(s/check bidi.schema/RoutePair app-routes)
 ;;(s/validate bidi.schema/RoutePair app-routes)
 
-;; == Route components
+(defmulti page-contents identity)
 
-(defn index []
+(defmethod page-contents :index []
   [:span
    [:h1 "Routing example: Index"]
    [:ul
     [:li [:a {:href (bidi/path-for app-routes :section-a) } "Section A"]]
     [:li [:a {:href (bidi/path-for app-routes :section-b) } "Section B"]]
+    [:li [:a {:href (bidi/path-for app-routes :missing-route) } "Missing-route"]]
     [:li [:a {:href "/borken/link" } "Borken link"]]]])
 
-(defn section-a []
+(defmethod page-contents :section-a []
   [:span
    [:h1 "Routing example: Section A"]
    [:ul (map (fn [item-id]
                [:li {:key (str "item-" item-id)}
-                [:a {:href (bidi/path-for app-routes :section-a-item :item-id item-id)} "Item: " item-id]])
+                [:a {:href (bidi/path-for app-routes :a-item :item-id item-id)} "Item: " item-id]])
              (range 1 6))]])
 
-(defn a-item []
-  [:span
-   [:h1 (str "Routing example: Section A, item " (:item-id (session/get :route-params)))]
-   [:p [:a {:href (bidi/path-for app-routes :section-a)} "Back to Section A"]]])
+(defmethod page-contents :a-item []
+  (let [routing-data (session/get :route)
+        item (get-in routing-data [:route-params :item-id])]
+    [:span
+     [:h1 (str "Routing example: Section A, item " item)]
+     [:p [:a {:href (bidi/path-for app-routes :section-a)} "Back to Section A"]]]))
 
-(defn section-b []
+(defmethod page-contents :section-b []
   [:span
    [:h1 "Routing example: Section B"]])
 
-(defn four-o-four []
+(defmethod page-contents :four-o-four []
+  "Non-existing routes go here"
   [:span
-   [:h1 "404"]
-   [:p "What you are looking for, "]
-   [:p "I do not have."]])
+   [:h1 "404: It is not here"]
+   [:pre.verse
+    "What you are looking for,
+I do not have.
+How could I have,
+what does not exist?"]])
 
-;; == /Route components
-
-;; Map from route keywords to reagent components
-(def page-components {:index {:component index}
-                      :section-a {:component section-a}
-                      :section-a-item {:component a-item}
-                      :section-b {:component section-b}
-                      :four-o-four {:component four-o-four}})
+(defmethod page-contents :default []
+  "Configured routes, missing an implementation, go here"
+  [:span
+   [:h1 "404: My bad"]
+   [:pre.verse
+    "This page should be here,
+but I never created it."]])
 
 (defn page []
-  [:div
-   [:p [:a {:href (bidi/path-for app-routes :index) } "Go home"]]
-   [:hr]
-   [(:component ((session/get :current-page) page-components))] ;; <- This will "dispatch" a new "page" when  the session reagent/atom changes.
-   ])
+  (fn []
+    (let [page (:current-page (session/get :route))]
+      [:div
+       [:p [:a {:href (bidi/path-for app-routes :index) } "Go home"]]
+       [:hr]
+       (page-contents page)
+       [:hr]
+       [:p "(Using "
+        [:a {:href "https://reagent-project.github.io/"} "Reagent"] ", "
+        [:a {:href "https://github.com/juxt/bidi"} "Bidi"] " & "
+        [:a {:href "https://github.com/venantius/accountant"} "Accountant"]
+        ")"]])))
 
 
 (defn on-js-reload []
@@ -81,8 +97,8 @@
                    (let [match (bidi/match-route app-routes path)
                          current-page (:handler match)
                          route-params (:route-params match)]
-                     (session/put! :current-page current-page) ;; <- De-refed in the [page] component, triggering a "dispatch".
-                     (session/put! :route-params route-params))) ;; To be de-refed by any component that needs them.
+                     (session/put! :route {:current-page current-page
+                                           :route-params route-params})))
     :path-exists? (fn [path]
                     (boolean (bidi/match-route app-routes path)))})
   (accountant/dispatch-current!)
