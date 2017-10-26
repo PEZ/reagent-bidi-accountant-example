e7aa1e8b471ddf03627a8f625925d3d6fba1ac6a
Peter Strömberg <pez@pezius.com>
2017-10-26 22:45:24 +0200

    Fix funny a-item paths bug

--- src/routing_example/core.cljs
+++ src/routing_example/core.cljs
@@ -13,7 +13,7 @@
 (def app-routes
   ["/" {"" :index
         "section-a" {"" :section-a
-                     [:item-id "/item-"] :a-item}
+                     ["/item-" :item-id] :a-item}
         "section-b" :section-b
         "missing-route" :missing-route
         true :four-o-four}])
